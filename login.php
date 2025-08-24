28c7f9bf9098942f2bf9f167d5a5c30751fbef59
hamdy hosny <77945384+Mady4host@users.noreply.github.com>
2025-08-24 22:04:04 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <title>تسجيل الدخول بفيسبوك</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container text-center" style="margin-top:120px;max-width:530px;">
+        <h2 class="mb-4 text-primary">تسجيل دخول عبر فيسبوك</h2>
+        <a href="<?= $login_url ?>" class="btn btn-primary btn-lg">
+            <img src="https://www.facebook.com/images/fb_icon_325x325.png" width="32" style="vertical-align:middle;margin-left:7px;">
+            سجّل دخول بحسابك فيسبوك
+        </a>
+    </div>
+</body>
+</html>
